a11111a6a222029f0a3e59394eee837abb406807
Mamun <mamunrushdi@gmail.com>
2024-07-02 23:48:46 +0300

    Lab1 has been completed.

---
+++ src/main/resources/store/insertion.sql
@@ -0,0 +1,5 @@
+insert into Products (article_id, product_name, product_color, prodcut_price, product_left) values(3251615, 'Стол кухонный', 'белый', 8000, 12);
+insert into Products (article_id, product_name, prodcut_price, product_left) values(3251616,'Стол кухонный', 8000, 15);
+insert into Products (article_id, product_name, product_color, prodcut_price, product_left) values(3251619, 'Стул столовый с высокой спинкой', 'белый', 3500, 37);
+insert into Products values(3251617, 'Стул столовый', 'гусарский орех', 4000, 0);
+insert into Products (article_id, product_name, product_color, prodcut_price, product_left) values(3251620, 'Стул столовый с высокой спинкой', 'коричневый', 3500, 52);
